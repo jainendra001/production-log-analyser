e2fe55d887f0ceb513d1b3768ca166aab1684278
Jainendra Tripathy <jainendratripathy@gmail.com>
2026-01-14 17:43:59 +0530

    add the dictionary as the logic

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,18 +1,31 @@
 import os
 FILE_PATH="logs/app.log"
-if os.path.exists(FILE_PATH):
-  # logFile=open(FILE_PATH,"r")
+if os.path.exists(FILE_PATH) :
   with open(FILE_PATH,"r") as logFile:
     lines=logFile.readlines()
-else:
-  print('The logs are not available.')
-  
-# lines=logFile.readlines()
-# logFile.close()
-ERROR_COUNT=0
-for line in lines :
-  if 'ERROR' in line :
-    ERROR_COUNT+=1
 
-print(f"The total Errors are : {ERROR_COUNT}")
+service_errors={}
+total_errors=0
+for line in lines:
+  if "ERROR" in line:
+    total_errors+=1
+    if "service=" in line:
+      start_index=line.find("service=")
+      service_start=line[start_index+8:]
+      service_name=service_start.split()[0]
+      if service_name in service_errors:
+        service_errors[service_name]+=1
+      else:
+        service_errors[service_name]=1
+    else:
+      if "unkown" in service_errors:
+        service_errors["unkown"]+=1
+      else:
+        service_errors["unkown"]=1
 
+print(f"Total errors: {total_errors}")
+print("\nErrors by service:")
+
+for service_name,error_count in service_errors.items():
+  print(f"{service_name}:{error_count}")
+      
